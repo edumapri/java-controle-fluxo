a13e6578c4b181250738f33d12afbbf76d5eb830
Eduardo O.S <edumapri@gmail.com>
2023-07-30 20:22:24 -0300

    update

---
+++ fluxos/src/ExemploFor3Each.java
@@ -0,0 +1,9 @@
+public class ExemploFor3Each {
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        for (String aluno : alunos) {
+            System.out.println(alunos);
+        }
+    }
+}

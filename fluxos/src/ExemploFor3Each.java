f0b494d6a778eecf01b5f959ff25b062f2968244
Eduardo O.S <edumapri@gmail.com>
2023-07-30 20:31:17 -0300

    update

--- fluxos/src/ExemploFor3Each.java
+++ fluxos/src/ExemploFor3Each.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExemploFor3Each {
     public static void main(String[] args) {
-        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+        String[] alunos = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
 
         for (String aluno : alunos) {
             System.out.println(aluno);
